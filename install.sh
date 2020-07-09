d74729c32fa650109df0a31d3bbdae6425ca3747
willnilges <wdn5796@rit.edu>
2020-07-09 18:30:05 -0400

    Add the ability to install for neovim

--- install.sh
+++ install.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
-if [ -d "~/.vim/ftdetect" ] && [ -d "~/.vim/syntax"]; then
-    cp vim/ftdetect/dnd.vim ~/.vim/ftdetect
-    cp vim/syntax/dndnv.vim ~/.vim/syntax/dndnv.vim
-else
-    mkdir ~/.vim
-    cp -r vim/ftdetect ~/.vim
-    cp -r vim/syntax ~/.vim
+editor_of_choice=""
+
+if [ -d "$HOME/.config/nvim" ]; then
+    while [ ! "$editor_of_choice" = "y" ] || [ ! "$editor_of_choice" = "n" ]; do
+        echo 'You appear to have neovim installed. Install for neovim? (y/n)'
+        read -r editor_of_choice
+        if [ "$editor_of_choice" = "y" ]; then
+            if [ -d "$HOME/.config/nvim/ftdetect" ] && [ -d "$HOME/.config/nvim/syntax" ]; then
+                cp vim/ftdetect/dnd.vim ~/.config/nvim/ftdetect/
+                cp vim/syntax/dndnv.vim ~/.config/nvim/syntax/
+            else
+                cp -r vim/ftdetect ~/.config/nvim/
+                cp -r vim/syntax ~/.config/nvim/
+            fi
+            echo -e '\033[1;32mInstalled for nvim. Check ~/.config/nvim/\033[0m'
+            break
+        elif [ "$editor_of_choice" = "n" ]; then
+            echo 'Not installing for nvim.'
+            break
+        fi
+    done
 fi
 
-echo 'Installation complete. Check your ~/.vim directory'
+editor_of_choice=""
+
+if [ -d "$HOME/.vim" ]; then
+    while [ ! "$editor_of_choice" = "y" ] || [ ! "$editor_of_choice" = "n" ]; do
+        echo 'You appear to have vim installed. Install for vim? (y/n)'
+        read -r editor_of_choice
+        if [ "$editor_of_choice" = "y" ]; then
+            if [ -d "$HOME/.vim/ftdetect" ] && [ -d "$HOME/.vim/syntax" ]; then
+                cp vim/ftdetect/dnd.vim ~/.vim/ftdetect/
+                cp vim/syntax/dndnv.vim ~/.vim/syntax/
+            else
+                mkdir ~/.vim
+                cp -r vim/ftdetect ~/.vim
+                cp -r vim/syntax ~/.vim
+            fi
+            echo -e '\033[1;32mInstalled for vim. Check ~/.vim/\033[0m'
+            break
+        elif [ "$editor_of_choice" = "n" ]; then
+            echo 'Not installing for vim.'
+            break
+        fi
+    done
+fi
+
+echo -e '\033[1mEnjoy my scripts! If you have any problems, open an issue @ https://github.com/willnilges/DNDNV
+
+Happy Adventuring!\033[0m
+
+(Kobold Gang)'
+
 exit 0
